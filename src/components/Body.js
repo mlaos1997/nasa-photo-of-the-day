2436bcad0e3e7dba719e674f6bd22fc2d0c6d182
Marcelo Laos <mlaos1997@gmail.com>
2021-08-08 21:37:27 -0400

    day 2 mvp complete

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,71 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const StyledBody = styled.div`
+position: relative;
+
+div {
+    margin: 2%;
+    position: absolute;
+    top: 71%;
+    left: 50%;
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+}
+
+p { color: #685206; 
+font-family: 'Helvetica Neue', sans-serif; 
+font-size: 24px; 
+line-height: 24px; 
+margin: 90px auto 0px; 
+text-align: justify; 
+text-justify: inter-word; 
+width: 80%;
+}
+
+h2 {
+    color: #111; 
+    font-family: 'Helvetica Neue', sans-serif; 
+    font-size: 25px; 
+    font-weight: bold; 
+    letter-spacing: -1px; 
+    line-height: 1; 
+    text-align: center;
+}
+
+img {
+    display: block;
+    margin: 0 auto;
+    border: 3px solid transparent;
+	background: linear-gradient(#000, #000) padding-box, radial-gradient(farthest-corner at var(--x) var(--y), #00C9A7, #845EC2) border-box;
+}
+
+button {
+    --x: 50%;
+	--y: 50%;    
+	appearance: none;
+	padding: 1em 2em;
+	color: #DDDDDD;
+	pointer-events: none;
+	outline: none;
+    border-radius: 0px;
+    font-size: 18px;
+
+	border: 3px solid transparent;
+	background: linear-gradient(#000, #000) padding-box, radial-gradient(farthest-corner at var(--x) var(--y), #00C9A7, #845EC2) border-box;
+}
+
+p {
+}
+
+`
 export default function Body(props) {
     const { picture, serviceVersion, explanation } = props;
-    return <div>
+    return <StyledBody>
         <img src={picture} />
-        <p>{serviceVersion}</p>
+        <div>
+            <button src="#">Service Version: {serviceVersion}</button>
+        </div>
         <p>{explanation}</p>
-    </div>
+    </StyledBody>
 }
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const StyledTitle = styled.div`
+padding: .01% 0;
+
+h1 {
+    color: #111; 
+    font-family: 'Helvetica Neue', sans-serif; 
+    font-size: 75px; 
+    font-weight: bold; 
+    letter-spacing: -1px; 
+    line-height: 1; 
+    text-align: center;
+}
+
+`
 export default function Title(props) {
-    return <div>{props.header}</div>
+    return (
+        <StyledTitle>
+            <h1>{props.header}</h1>
+        </StyledTitle>
+    )
 }
